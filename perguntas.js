03ff81bd90a8dec7fb4c933551353f26a36d9f3b
998852269 <160133134+998852269@users.noreply.github.com>
2024-11-05 09:54:31 -0300

    Update perguntas.js

--- perguntas.js
+++ perguntas.js
@@ -1,23 +1,23 @@
 criaCartao(
-    'Programação',
-    'O que é Python?',
-    'O Python é uma linguagem de programação'
+    'ciencia ',
+    'qual planeta mais proximo do sol?',
+    'mercurio'
 )
 
 criaCartao(
     'Geografia',
-    'Qual a capital da França?',
-    'A capital da França é Paris'
+    'Qual é o único continente com terra nos quatro hemisférios?',
+    'africa'
 )
 
 criaCartao(
-    'Programação',
-    'O que é uma função?',
-    'Uma função é um bloco de código que executa alguma tarefa'
+    'historia',
+    ' Onde surgiram os primeiros ancestrais do homem ?',
+    'africa'
 )
 
 criaCartao(
-    'Lingua inglesa',
-    'Como se diz oi em Inglês?',
-    'Oi em ingles é HI (RAI)'
+    'matematica',
+    'quanto é 2+2?',
+    '4'
 )
